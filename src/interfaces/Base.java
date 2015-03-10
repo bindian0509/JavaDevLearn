c44f172fe1ac1e779ab4b885989890a5b6ee3830
BHARAT VERMA <bharat.verma@naukri.com>
2015-03-10 14:27:06 +0530

    changes for different dropdown for sms and mails in resume freshness

---
+++ src/interfaces/Base.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaces;
+
+/**
+ *
+ * @author bharat
+ */
+public interface Base {
+    
+    boolean m1 ();
+    byte m2 (short s);
+    
+}
---
+++ src/interfaces/Base2.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaces;
+
+/**
+ *
+ * @author bharat
+ */
+public interface Base2 extends Base{
+    public void main(String [] args);
+    boolean setFlag(Boolean [] test);
+}
